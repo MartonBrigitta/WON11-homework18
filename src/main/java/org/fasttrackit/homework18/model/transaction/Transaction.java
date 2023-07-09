0d9ab401708d5456c5b82d5871b906630fd3ad9d
BRIGI MARTON <brigi.marton397@gmail.com>
2023-07-09 16:17:05 +0300

    Homework18 update cu db

--- src/main/java/org/fasttrackit/homework18/model/transaction/Transaction.java
+++ src/main/java/org/fasttrackit/homework18/model/transaction/Transaction.java
@@ -1,5 +1,6 @@
 package org.fasttrackit.homework18.model.transaction;
 
+import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -7,14 +8,21 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Getter
 @Builder
+@Entity
 public class Transaction {
+    @Id
+    @GeneratedValue
 
     //id
     //product
     //type: SELL/Buy ( daca pot fi de doua feluri atunci facem ENUM)
     //amount double
-    private final long id;
+    private final Long id;
+    @Column
     private final String product;
+    @Column
+    @Enumerated(value = EnumType.STRING)
     private final TransactionType type;
+    @Column
     private final double amount;
 }
